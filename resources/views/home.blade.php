ec732981e50e1450bcb6df616c67b1927a391cab
Leon <yltan828@gmail.com>
2020-09-21 22:41:56 -0500

    remade homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!--The home page that HomeController calls-->
+
+<!--See master.blade.php-->
+@extends('master')
+
+@section('title', 'Homepage')
+
+@section('content')
+
+    <h1>Content!</h1>
+
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,19 @@
+<!--This is the master template-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+<h1>Zip Code to Location</h1>
+<p>Please type in the Zip code to show information</p>
+
+    @yield('content')
+
+</body>
+</html>
