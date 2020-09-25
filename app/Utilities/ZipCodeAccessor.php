3361d7521984d8cf916e720207aa10e578580b00
Leon <yltan828@gmail.com>
2020-09-24 23:53:11 -0500

    update api key

--- app/Utilities/ZipCodeAccessor.php
+++ app/Utilities/ZipCodeAccessor.php
@@ -11,7 +11,7 @@ class ZipCodeAccessor
     {
         $client = new Client();
 
-        $api_key = 'ycu3JwgT9YdhWrpYIeyVBo6bTbxgf7D3iwAJnSmgw10Mamw5lfTXXIHHdkJHif4b';
+        $api_key = 'T2FVoIDOIkc3gIlel9Ob2JkmXZ9Z3y4LZbKeaQkcSuObj11IasF9xDM1B5QQtFUj';
         $format = 'json';
         $units = 'degrees';
         $api_url = 'http://www.zipcodeapi.com/rest/'.$api_key.'/info.'.$format.'/'.$zip.'/'.$units;
